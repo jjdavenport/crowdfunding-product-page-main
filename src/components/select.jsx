d90f1ec4426bb59ae327b80b6daf34ee4d0ea20e
jjdavenport <jordanjamesdavenport@gmail.com>
2024-10-19 19:45:08 +0100

    add select

--- src/components/select.jsx
+++ src/components/select.jsx
@@ -1,12 +1,15 @@
 import Selected from "./selected";
 import closeIcon from "./assets/icon-close-modal.svg";
+import { useState } from "react";
 
 const Select = ({ onClick, onSelect }) => {
+  const [selected, setSelected] = useState(null);
+  const select = (i) => setSelected(i);
   return (
     <>
-      <dialog className="block">
-        <div className="flex">
-          <h3>Back this project</h3>
+      <dialog className="top-20 flex w-10/12 flex-col gap-4 rounded-lg p-8">
+        <div className="flex justify-between">
+          <h3 className="text-2xl font-bold">Back this project</h3>
           <button onClick={onClick}>
             <img src={closeIcon} />
           </button>
@@ -15,42 +18,48 @@ const Select = ({ onClick, onSelect }) => {
           Want to support us in bringing Mastercraft Bamboo Monitor Riser out in
           the world?
         </p>
-        <ul>
+        <ul className="flex flex-col gap-4">
           <Selected
+            selected={selected === "no reward"}
+            onSelect={() => select("no reward")}
             onClick={onSelect}
             title="Pledge with no reward"
             paragraph="Choose to support us
               without a reward if you simply believe in our project. As a backer, you
               will be signed up to receive product updates via email."
+            input="1"
           />
           <Selected
+            selected={selected === "bamboo stand"}
+            onSelect={() => select("bamboo stand")}
             onClick={onSelect}
-            title="Bamboo Stand Pledge $25 or more"
+            title="Bamboo Stand"
             pledge="25"
             paragraph="You get an ergonomic stand made of natural bamboo.
               You've helped us launch our promotional campaign, and you’ll be added to a
               special Backer member list."
             input="25"
-            left="101"
+            amount="101"
           />
           <Selected
+            selected={selected === "black edition"}
+            onSelect={() => select("black edition")}
             onClick={onSelect}
-            title="Black Edition Stand Pledge $75 or more"
+            title="Black Edition Stand"
             pledge="75"
             paragraph="You get a Black Special Edition
                   computer stand and a personal thank you. You’ll be added to our Backer
                   member list. Shipping is included."
             input="75"
-            left="64"
+            amount="64"
           />
           <Selected
-            onClick={onSelect}
             title="Mahogany Special Edition Pledge $200 or more"
             pledge="200"
             paragraph="You get two Special Edition
                 Mahogany stands, a Backer T-Shirt, and a personal thank you. You’ll be
                 added to our Backer member list. Shipping is included."
-            left="0"
+            amount="0"
           />
         </ul>
       </dialog>
--- src/components/selected.jsx
+++ src/components/selected.jsx
@@ -1,20 +1,58 @@
-const Selected = ({ title, pledge, paragraph, input, onClick }) => {
+const Selected = ({
+  title,
+  pledge,
+  paragraph,
+  input,
+  onClick,
+  amount,
+  onSelect,
+  selected,
+}) => {
   return (
     <>
-      <li>
-        <div>
-          <span>{title}</span>
-          {pledge && <span>pledge ${pledge} or more</span>}
-          <p>{paragraph}</p>
-        </div>
-        <div>
-          <span>Enter your ledge</span>
-          <div>
-            <input value={input} type="number" />
-            <button onClick={onClick}>Continue</button>
+      <button
+        onClick={onSelect}
+        className={`${amount === "0" ? "opacity-40" : null} ${selected ? "outline-moderateCyan" : "outline-darkGray"} flex flex-col gap-4 divide-y-2 rounded-lg text-start outline outline-2`}
+      >
+        <div className="flex flex-col gap-2 p-4">
+          <div className="flex items-center gap-2">
+            <div className="flex h-4 w-4 items-center justify-center rounded-full outline outline-[1px] outline-darkGray">
+              <div
+                className={`${selected ? "bg-moderateCyan" : null} h-[10px] w-[10px] rounded-full`}
+              ></div>
+            </div>
+            <span>{title}</span>
           </div>
+          {pledge && (
+            <span className="font-medium text-moderateCyan">
+              Pledge ${pledge} or more
+            </span>
+          )}
+          <p>{paragraph}</p>
+          {amount && <span>{amount} left</span>}
         </div>
-      </li>
+        {selected && input && (
+          <div className="flex w-full flex-col gap-1 p-4">
+            <span>Enter your pledge</span>
+            <div className="flex gap-4">
+              <label className="flex items-center gap-2 rounded-full p-2 text-darkGray outline outline-2 outline-darkGray">
+                $
+                <input
+                  className="w-20 focus:outline-none"
+                  value={input}
+                  type="number"
+                />
+              </label>
+              <button
+                className="rounded-full bg-moderateCyan p-3 font-bold text-white"
+                onClick={onClick}
+              >
+                Continue
+              </button>
+            </div>
+          </div>
+        )}
+      </button>
     </>
   );
 };
