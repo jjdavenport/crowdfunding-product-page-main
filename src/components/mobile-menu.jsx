e780496d2fb239369df9e57749a85647d9131fcd
jjdavenport <jordanjamesdavenport@gmail.com>
2024-10-24 14:26:08 +0100

    import react spring, add mobileMenu animation

--- src/components/mobile-menu.jsx
+++ src/components/mobile-menu.jsx
@@ -1,46 +1,47 @@
-import { useEffect, useState } from "react";
+import { useTransition, animated } from "@react-spring/web";
 
-const MobileMenu = () => {
-  const [active, setActive] = useState(false);
+const MobileMenu = ({ menu }) => {
+  const transitions = useTransition(menu, {
+    from: { transform: "scale(0.95)", opacity: 0 },
+    enter: { transform: "scale(1)", opacity: 1 },
+    leave: { transform: "scale(0.95)", opacity: 0 },
+    config: { tension: 170, friction: 26 },
+  });
 
-  useEffect(() => {
-    document.body.classList.add("overflow-hidden");
-    setActive(true);
-
-    return () => {
-      document.body.classList.remove("overflow-hidden");
-    };
-  }, []);
-
-  return (
-    <>
-      <div className="fixed inset-0 z-40 bg-black bg-opacity-50"></div>
-      <div
-        className={`absolute z-50 flex w-full flex-col transition-all duration-300 ease-in-out ${
-          active ? "scale-100 opacity-100" : "scale-95 opacity-0"
-        }`}
-      >
-        <dialog className="top-20 flex w-10/12 rounded-lg">
-          <ul className="flex flex-1 flex-col divide-y-[1px]">
-            <li className="p-4">
-              <a className="font-bold" href="#">
-                About
-              </a>
-            </li>
-            <li className="p-4">
-              <a className="font-bold" href="#">
-                Discover
-              </a>
-            </li>
-            <li className="p-4">
-              <a className="font-bold" href="#">
-                Get Started
-              </a>
-            </li>
-          </ul>
-        </dialog>
-      </div>
-    </>
+  return transitions(
+    (styles, item) =>
+      item && (
+        <>
+          <animated.div
+            className="fixed inset-0 z-40 bg-black bg-opacity-50"
+            style={{ opacity: styles.opacity }}
+          />
+          <animated.div
+            className="absolute z-50 flex w-full flex-col"
+            style={styles}
+          >
+            <dialog className="top-20 flex w-10/12 rounded-lg">
+              <ul className="flex flex-1 flex-col divide-y-[1px]">
+                <li className="p-4">
+                  <a className="font-bold" href="#">
+                    About
+                  </a>
+                </li>
+                <li className="p-4">
+                  <a className="font-bold" href="#">
+                    Discover
+                  </a>
+                </li>
+                <li className="p-4">
+                  <a className="font-bold" href="#">
+                    Get Started
+                  </a>
+                </li>
+              </ul>
+            </dialog>
+          </animated.div>
+        </>
+      ),
   );
 };
 
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -6,26 +6,25 @@ import closeIcon from "./assets/icon-close-menu.svg";
 
 const Nav = ({ desktop, select, complete }) => {
   const [menu, setMenu] = useState(false);
-  const toggle = () => setMenu(!menu);
+
+  const toggle = () => setMenu((prev) => !prev);
 
   if (desktop) {
     return (
-      <>
-        <nav className="item flex h-96 w-full justify-between bg-desktop p-10 font-medium text-white ~md/xl:~px-20/32">
-          <img className="h-6 object-contain" src={logo} />
-          <ul className="flex gap-8">
-            <li>
-              <a href="#">About</a>
-            </li>
-            <li>
-              <a href="#">Discover</a>
-            </li>
-            <li>
-              <a href="#">Get Started</a>
-            </li>
-          </ul>
-        </nav>
-      </>
+      <nav className="item flex h-96 w-full justify-between bg-desktop p-10 font-medium text-white ~md/xl:~px-20/32">
+        <img className="h-6 object-contain" src={logo} />
+        <ul className="flex gap-8">
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Discover</a>
+          </li>
+          <li>
+            <a href="#">Get Started</a>
+          </li>
+        </ul>
+      </nav>
     );
   }
 
@@ -43,7 +42,7 @@ const Nav = ({ desktop, select, complete }) => {
           <img className="object-contain" src={menu ? closeIcon : menuIcon} />
         </button>
       </nav>
-      {menu && <MobileMenu />}
+      <MobileMenu menu={menu} />
     </>
   );
 };
